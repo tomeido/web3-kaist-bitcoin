b36294085481634f05226e9b059d56174b7157ca
tomeido <tomeido@naver.com>
2023-04-13 11:25:22 +0000

    compil

--- contracts/KaistBitcoin.sol
+++ contracts/KaistBitcoin.sol
@@ -3,6 +3,8 @@ pragma solidity >=0.8.0;
 
 import "hardhat/console.sol";
 
+import {ECDSA} from "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
+
 contract KaistBitcoin {
     struct KaistBitcoinTx {
         address from;
@@ -14,11 +16,16 @@ contract KaistBitcoin {
 
     function validateTx(KaistBitcoinTx memory _tx) public pure returns (bool) {
         // verify signature
+        bytes32 txHash = _txHash(_tx);
+        bytes32 hashToSigh = hash("prefx" + txHash);
     }
 
     function _txHash(
         KaistBitcoinTx memory _tx
     ) internal pure returns (bytes32) {
         // return the hash of the transaction
+        return keccak256(abi.encodePacked(
+            _tx.from, _tx.to, _tx.amount, _tx.fee
+        ));
     }
 }
